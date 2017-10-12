1748a92699efe72de412833bcda24a04bc26b053
Sergio Maia <sergiomaia@gmail.com>
2017-10-11 23:50:53 -0300

    apdated seeds and readme files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+ @user = User.create(email: "john@doe.com", 
+                           password: "123456", 
+                           password_confirmation: "123456",
+                           first_name: "John",
+                           last_name: "Doe")
+
+puts "On user created."
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 posts have been created."
